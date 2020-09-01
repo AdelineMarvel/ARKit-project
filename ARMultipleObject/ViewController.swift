e7f038a5f11218a24f95ed8ed99e3ce142d3cc44
Alberta Adeline Marvel <arc.rayne7@gmail.com>
2020-09-01 10:35:41 +0700

    Initial Commit

---
+++ ARMultipleObject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARMultipleObject
+//
+//  Created by alberta adeline marvel on 01/09/20.
+//  Copyright © 2020 alberta adeline marvel. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
