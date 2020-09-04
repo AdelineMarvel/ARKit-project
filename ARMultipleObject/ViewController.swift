d59635162c7974a5797cb9def9ad1ec088d3bcb9
Alberta Adeline Marvel <arc.rayne7@gmail.com>
2020-09-04 10:45:55 +0700

    AR Project NC3

--- ARMultipleObject/ViewController.swift
+++ ARMultipleObject/ViewController.swift
@@ -10,53 +10,76 @@ import UIKit
 import RealityKit
 import SnapKit
 import ARKit
+import Combine
 
 class ViewController: UIViewController, ConstraintRelatableTarget {
     
     @IBOutlet var arView: ARView!
     
+    let config = ARWorldTrackingConfiguration()
+    
+    lazy var loadingView: UIActivityIndicatorView = {
+        let loadingView = UIActivityIndicatorView(style: .large)
+        loadingView.color = .white
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(loadingView)
+        
+        loadingView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loadingView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        return loadingView
+    }()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //buttons
+        //set up configuration
+        arView.automaticallyConfigureSession = false
+        config.planeDetection = .horizontal
+        config.environmentTexturing = .automatic
+        arView.session.run(config)
+        
+        //call func to remove object
+        arView.enableObjectRemoval()
+        
+        
+        
+        
         let contentView = UIView()
         contentView.backgroundColor = .separator
         contentView.layer.cornerRadius = 10
         
         
+        func buttonDetail() -> UIButton {
+            let button = UIButton()
+            button.layer.cornerRadius = 10
+            button.imageView!.layer.cornerRadius = 10
+            button.clipsToBounds = true
+            button.addTarget(self, action: #selector(loadModel(sender:)), for: .touchUpInside)
+            return button
+        }
         
-        let buttonChair = UIButton()
+        
+        let buttonChair = buttonDetail()
+        buttonChair.tag = 1
         buttonChair.setBackgroundImage(UIImage (named: "chair"), for: UIControl.State.normal)
-        buttonChair.layer.cornerRadius = 10
-        buttonChair.imageView!.layer.cornerRadius = 10
-        buttonChair.clipsToBounds = true
-        buttonChair.addTarget(self, action: #selector(loadChair), for: .touchUpInside)
         
-        
-        let buttonTeapot = UIButton()
+        let buttonTeapot = buttonDetail()
+        buttonTeapot.tag = 2
         buttonTeapot.setBackgroundImage(UIImage (named: "teapot"), for: UIControl.State.normal)
-        buttonTeapot.layer.cornerRadius = 10
-        buttonTeapot.imageView!.layer.cornerRadius = 10
-        buttonTeapot.clipsToBounds = true
         
         
-        
-        
-        let buttonRetroTV = UIButton()
+        let buttonRetroTV = buttonDetail()
+        buttonRetroTV.tag = 3
         buttonRetroTV.setBackgroundImage(UIImage (named: "retroTV"), for: UIControl.State.normal)
-        buttonRetroTV.layer.cornerRadius = 10
-        buttonRetroTV.imageView!.layer.cornerRadius = 10
-        buttonRetroTV.clipsToBounds = true
         
-        
-        
-        
-        let buttonGramophone = UIButton()
+        let buttonGramophone = buttonDetail()
+        buttonGramophone.tag = 4
         buttonGramophone.setBackgroundImage(UIImage (named: "gramophone"), for: UIControl.State.normal)
-        buttonGramophone.layer.cornerRadius = 10
-        buttonGramophone.imageView!.layer.cornerRadius = 10
-        buttonGramophone.clipsToBounds = true
+        
+        
         
         
         
@@ -68,9 +91,10 @@ class ViewController: UIViewController, ConstraintRelatableTarget {
         contentView.addSubview(buttonGramophone)
         
         
-        //making constraint
+        
+        //        making constraint
         contentView.snp.makeConstraints { (make) in
-            make.width.equalTo(414)
+            make.width.equalTo(arView)
             make.height.equalTo(125)
             make.bottom.equalTo(arView)
         }
@@ -102,33 +126,92 @@ class ViewController: UIViewController, ConstraintRelatableTarget {
         buttonGramophone.snp.makeConstraints { (make) in
             make.width.equalTo(80)
             make.height.equalTo(80)
-            make.right.equalTo(-17)
+            make.left.equalTo(317)
             make.bottom.equalTo(-30)
             
         }
         
-        //set up config
-        let arConfiguration = ARWorldTrackingConfiguration()
-        arConfiguration.planeDetection = .horizontal
-        arView.session.run(arConfiguration)
         
     }
     
-    @objc private func loadChair() {
+    
+    
+    
+    @objc func loadModel(sender:UIButton) {
         
-        let chairModelEntity = try! ModelEntity.load(named: "chairReality")
-        chairModelEntity.generateCollisionShapes(recursive: true)
+        //buat switch untuk nama tiap button biar bisa load modelnyaa
+        var modelName = ""
+        switch sender.tag {
+        case 1:
+            modelName = "chairReality"
+        case 2:
+            modelName = "teapotReality"
+        case 3:
+            modelName = "retroTVReality"
+        case 4:
+            modelName = "gramophoneReality"
+        default:
+            modelName = ""
+        }
         
+        //Show loading Indicatior
+        loadingView.isHidden = false
         
-        let anchorEntity = AnchorEntity(plane: .horizontal)
-        anchorEntity.addChild(chairModelEntity)
+        let anchor = AnchorEntity(plane: .horizontal)
+        arView.scene.addAnchor(anchor)
         
+        // Load model Asynchoronously so the camera doesn't have to freeze
+        // Using Combine framework
+        var cancellableRequest: AnyCancellable? = nil
+        cancellableRequest = ModelEntity.loadModelAsync(named: modelName)
+            .sink(receiveCompletion: { error in
+                print("Unexpected error: \(error)")
+                // cancel the request
+                self.loadingView.isHidden = true
+                cancellableRequest?.cancel()
+            }, receiveValue: { entity in
+                entity.generateCollisionShapes(recursive: true)
+                
+                anchor.addChild(entity)
+                
+                self.arView.installGestures(.all, for: entity)
+                // finish the request
+                self.loadingView.isHidden = true
+                cancellableRequest?.cancel()
+            })
         
-//        let entityBounds = anchorEntity.visualBounds(relativeTo: chairModelEntity)
-//        chairModelEntity.collision = CollisionComponent(shapes: [ShapeResource.generateBox(size: entityBounds.extents).offsetBy(translation: entityBounds.center)])
-//
-//        arView.installGestures(.all, for: anchorEntity as! HasCollision )
-        arView.scene.addAnchor(anchorEntity)
         
     }
+    
+    
 }
+
+extension ARView {
+    
+    
+    func enableObjectRemoval() {
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
+        self.addGestureRecognizer(longPressGesture)
+    
+        
+    }
+    
+    @objc func handleLongPress(recognizer: UILongPressGestureRecognizer) {
+        
+        let location = recognizer.location(in: self)
+        
+        
+        if let entity = self.entity(at: location) {
+            if let anchorEntity = entity.anchor {
+                anchorEntity.removeFromParent()
+            }
+            
+        }
+        
+    }
+    
+}
+
+
+
+
